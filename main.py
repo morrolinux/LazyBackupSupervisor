5e2018250c9a969a3ce003338026829291dc4f40
Moreno Razzoli <morrolinux@gmail.com>
2021-11-09 18:58:17 +0100

    manual check works

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from flask import Flask, jsonify, request
 from datetime import datetime
 
 
-last_op_template = {"last_copy": None, "last_ver": None, "last_sync": None}
+last_op_template = {"copy": None, "ver": None, "sync": None}
 status_template = {"copy": None, "ver": None, "sync": None}
-settings_template = {"copy_interval": 60*24, "ver_interval": 60*24, "sync_interval": 60*72}
-stations = {"Pi4": {"master": {"settings": settings_template, "status": status_template, "last_op": last_op_template}}}
+interval_template = {"copy": 60*24, "ver": 60*24, "sync": 60*72}
+stations = {"Pi4": {"master": {"interval": interval_template, "status": status_template, "last_op": last_op_template}}}
 
 
 def traverse(d, path):
@@ -27,6 +27,7 @@ def index():
 
 @app.route('/status', methods=['GET'])
 def status_all():
+    check_elapsed()
     return jsonify({'stations': stations})
 
 @app.route('/status/<path:path>', methods=['GET'])
@@ -41,9 +42,9 @@ def create():
     station = request.form.get('station')
 
     if station in stations.keys():
-        stations[station][repo] = {"status": status_template, "settings": settings_template, "last_op": last_op_template}
+        stations[station][repo] = {"status": status_template, "interval": interval_template, "last_op": last_op_template}
     else:
-        stations[station] = {repo: {"status": status_template, "settings": settings_template, "last_op": last_op_template}}
+        stations[station] = {repo: {"status": status_template, "interval": interval_template, "last_op": last_op_template}}
 
     return jsonify({"result": "OK"})
     
@@ -61,7 +62,11 @@ def update():
 
 
 def is_elapsed(station, repo, operation, time_limit):
-    last = datetime.strptime(stations[station][repo]["last_op"][operation], '%Y-%m-%d %H:%M:%S.%f')
+    try:
+        last = datetime.strptime(stations[station][repo]["last_op"][operation], '%Y-%m-%d %H:%M:%S.%f')
+    except Exception:
+        return True
+
     now = datetime.now()
     delta = now - last
     delta_hours = divmod(delta.seconds, 60)[0]
@@ -70,11 +75,17 @@ def is_elapsed(station, repo, operation, time_limit):
 
 def check_elapsed():
     for station in stations:
-        for repo in station.keys():
-            pass
+        for repo in stations[station]:
+            for operation in stations[station][repo]["last_op"]:
+                ts = stations[station][repo]["last_op"][operation]
+                time_limit = stations[station][repo]["interval"][operation]
+                error = is_elapsed(station, repo, operation, time_limit)
+                status = "KO" if error else "OK"
+                stations[station][repo]["status"][operation] = not error
 
 
 if __name__ == "__main__":
     from waitress import serve
+    check_elapsed()
     serve(app, host="0.0.0.0", port=8080)
     # app.run(debug=True)
